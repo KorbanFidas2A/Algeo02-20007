8e380594747042905cef3b1f861a001939e7b5dd
roastland <tfpruslanali@gmail.com>
2021-11-10 12:15:13 +0700

    new test

---
+++ App/api/tes.py
@@ -0,0 +1,67 @@
+import numpy as np
+import cv2
+from PIL import Image
+
+def eigenvalue(A, v):
+    val = A @ v / v
+    return val[0]
+
+def svd_dominant_eigen(A, epsilon=0.01):
+    """returns dominant eigenvalue and dominant eigenvector of matrix A"""
+    n, m = A.shape
+    k=min(n,m)
+    v = np.ones(k) / np.sqrt(k)
+    if n > m:
+        A = A.T @ A
+    elif n < m:
+        A = A @ A.T
+    
+    ev = eigenvalue(A, v)
+
+    while True:
+        Av = A@ v
+        v_new = Av / np.linalg.norm(Av)
+        ev_new = eigenvalue(A, v_new)
+        if np.abs(ev - ev_new) < epsilon:
+            break
+
+        v = v_new
+        ev = ev_new
+
+    return ev_new, v_new
+
+def svd(A, k=None, epsilon=1e-10):
+    """returns k dominant eigenvalues and eigenvectors of matrix A"""
+    A = np.array(A, dtype=float)
+    n, m = A.shape
+        
+    svd_so_far = []
+    if k is None:
+        k = min(n, m)
+
+    for i in range(k):
+        matrix_for_1d = A.copy()
+
+        for singular_value, u, v in svd_so_far[:i]:
+            matrix_for_1d -= singular_value * np.outer(u, v)
+
+        if n > m:
+            _, v = svd_dominant_eigen(matrix_for_1d, epsilon=epsilon)  # next singular vector
+            u_unnormalized = A @ v
+            sigma = np.linalg.norm(u_unnormalized)  # next singular value
+            u = u_unnormalized / sigma
+        else:
+            _, u = svd_dominant_eigen(matrix_for_1d, epsilon=epsilon)  # next singular vector
+            v_unnormalized = A.T @ u
+            sigma = np.linalg.norm(v_unnormalized)  # next singular value
+            v = v_unnormalized / sigma
+
+        svd_so_far.append((sigma, u, v))
+
+    singular_values, us, vs = [np.array(x) for x in zip(*svd_so_far)]
+    return singular_values, us.T, vs
+
+
+img = cv2.imread("wkwkwk.JPG")
+np_img = np.array(img)
+cv2.imwrite("opncv_sample.JPG", img)
